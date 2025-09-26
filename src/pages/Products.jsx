0d2f7d1a5a2226493cbd23481589c42fc610cb96
nikhilfiction <nikhilsahu3993@gmail.com>
2025-09-26 13:36:59 +0530

    Added products list

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,14 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../features/productSlice";
+//
 const Products = () => {
 const dispatch= useDispatch();
 const {items: products, loading, error} = useSelector(
     (state) => state.products
 )
 useEffect(()=> {
-    dispatch(fetchProducts)
+    dispatch(fetchProducts())
 }, [dispatch])
 
 if(loading) return <div className= "container mx-auto">Loading...</div>
@@ -19,9 +20,21 @@ if(error) return <div className="container mx-auto">Error: {error}</div>
     return (
 
         <div className="container mx-auto">
-        <h1 className="text3x1 font-bold mb-4">Welcome to the store</h1>
-        <p className="text-gray-700 mb-6">Product page!</p>
-    </div>
+        <h2 className="text-2x1 font-bold mb-4">Products</h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            {products.map((product)=> (
+                <div key={product._id} className="bg-white p-4 shadowrounded flex flex-col">
+                    <img src= {product.image} alt={product.title} className="h-48 object-cover mb-4 rounded"/>
+                    <h3 className="text-lg font-bold">{product.title}</h3>
+                    <p className="text-gray-600 my-2">{product.price}</p>
+                    <p className="text-gray-500 mb-2">{product.description}</p>
+                    <button className="mt-auto bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">Add to cart</button>
+                </div>
+            ))}
+        </div>
+         
+        
+        </div>
     );
 };
 
