75d4701c6a7851ae0b301b88e7ded1b1a3e0f058
nikhilfiction <nikhilsahu3993@gmail.com>
2025-10-02 02:15:54 +0530

    Added the auth store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import  productReducer  from "../features/productSlice";
 import homepageReducer from '../features/homepageSlice';
+import authReducer from '../features/authSlice';
 
 export const store= configureStore({
     reducer: {
         products: productReducer,
         homepage: homepageReducer,
+        auth: authReducer
     },
 });
 
