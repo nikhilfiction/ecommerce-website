edbaf5b36842393877c668c99d5a5ccbfb0a9b79
nikhilfiction <nikhilsahu3993@gmail.com>
2026-09-09 00:34:20 +0530

    Refactor App component layout and styles

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,9 @@ export default function App() {
     //Layout
     //Header
     <Router>
-     <div className="flex flex-col min-h-screen">
+     <div className="flex flex-col min-h-screen bg-gray-50">
       <Header/>
-        <main className="flex grow p-4 bg-gray-100 justify-center items-center">
+        <main className="grow w-full py-6">
      <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/products" element={<Products />}></Route>
@@ -27,42 +27,9 @@ export default function App() {
       <Footer />
 
      </div>
-      
+
     </Router>
-      
-    
+
+
   );
 }
-
-
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
-// import Home from "./pages/Home";
-// import Products from "./pages/Products";
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
-// export default function App() {
-//   return (
-//     //Layout
-//     //Header
-//     <Router>
-//      <div className="flex flex-col min-h-screen">
-//       <Header/>
-//         <main className="flex grow p-4 bg-gray-100 justify-center items-center">
-//      <Routes>
-//         <Route path="/" element={<Home />}></Route>
-//         <Route path="/products" element={<Products />}></Route>
-//         <Route path="/register" element={<Register/>}></Route>
-//         <Route path="/login" element={<Login/>}></Route>
-//       </Routes>
-//       </main>
-//       <Footer />
-
-//      </div>
-      
-//     </Router>
-      
-    
-//   );
-// }
